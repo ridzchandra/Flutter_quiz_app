c55639d02505ea3b28eb78c5142cf6aa5db1dcd5
Ridz Chandra <ridzchandra9@gmail.com>
2020-08-25 00:48:04 +1000

    Day-1

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(QuizApp());
+}
+
+class QuizApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Text("Fatass!!!"),
+    );
+  }
+}
